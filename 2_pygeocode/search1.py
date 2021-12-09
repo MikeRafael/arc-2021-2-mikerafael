3d34590223570ae83ebd3c79c89b4e1b62dd75fb
Mike Rafael <mikerafaelpl10@hotmail.com>
2021-12-09 14:11:41 -0300

    Search1 2.0.

--- 2_pygeocode/search1.py
+++ 2_pygeocode/search1.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pytho3
 
 from geopy.geocoders import Nominatim
-
 
 if __name__ == '__main__':
     address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
     user_agent = 'Search1'
-    location = Nominatim(user_agent=user_agent).geocode(address)
-    print(location.latitude, location.longitude)
+    location = Nominatim(user_agent=user_agent).geocode(address, exactly_one=False, limit=3, addressdetails=True)
+    cep1: str = location[0].raw['address']['postcode']
+    cep2: str = location[1].raw['address']['postcode']
+    print("Endereço buscado", address, "\nResultado 1: \n CEP: ", cep1, "\nLatitude e Longitude: ",
+          location[1].latitude, location[1].longitude, "\nCEP: ", cep2, "\nLatitude e Longitude: ",
+          location[0].latitude, location[0].longitude)
