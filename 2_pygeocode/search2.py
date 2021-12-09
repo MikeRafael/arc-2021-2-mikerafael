bd1d315b7d1bada24f12e50a776e4410c51b929b
Mike Rafael <mikerafaelpl10@hotmail.com>
2021-12-09 14:23:16 -0300

    Search2 2.0.

--- 2_pygeocode/search2.py
+++ 2_pygeocode/search2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import requests
-
+from geopy.geocoders import Nominatim
 
 def geocode(address):
     base = 'https://nominatim.openstreetmap.org/search'
@@ -9,7 +9,12 @@ def geocode(address):
     headers = {'User-Agent': user_agent}
     response = requests.get(base, params=parameters, headers=headers)
     reply = response.json()
-    print(reply[0]['lat'], reply[0]['lon'])
+    location = Nominatim(user_agent=user_agent).geocode(address, exactly_one=False, limit=3, addressdetails=True)
+    cep1: str = location[0].raw['address']['postcode']
+    cep2: str = location[1].raw['address']['postcode']
+    print("Endereço buscado", address, "\nResultado 1: \n CEP: ", cep1, "\nLatitude e Longitude: ",
+          reply[0]["lat"], reply[0]["lon"], "\nCEP: ", cep2, "\nLatitude e Longitude: ",
+          reply[1]["lat"], reply[1]["lon"])
 
 
 if __name__ == '__main__':
